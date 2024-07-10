89fb2f09da98e699adf4a6d8325ef52ef867e9f8
TanishaVijayMalwadkar <tanishamalwadkar70@gmail.com>
2024-07-10 14:27:30 +0530

    demo file sending

--- crud.py
+++ crud.py
@@ -1,23 +1,21 @@
 import csv
-
-csv_file='hostel.csv'
-CSV_File='Hostel1.csv'
+CSV_File='students.csv'
 def read_csv():
-    with open(csv_file,mode='r',newline='') as file:
+    with open(CSV_File,mode='r',newline='') as file:
         reader=csv.DictReader(file)
         return list(reader)
     
-  with open(CSV_File,mode='r',newline='')as file:
-    reader=csv.DictReader(file)
-    return list(reader)
+    with open(CSV_File,mode='r',newline='')as file:
+        reader=csv.DictReader(file)
+        return list(reader)
 
 def write_csv(data):
-    with open(csv_file,mode='w',newlinw='') as file:
+    with open(CSV_File,mode='w',newlinw='') as file:
         write=csv.DictReader(file,filednames=['name','room_no','mobile_no','date','ragistor_no','fees','adress'])
         writer.writeheader()
         writer.writerows(data)
 
-def add(name,room_no,mobile_no,date,ragistor_name,fees,adress):
+def add(name,room_no,mobile_no,date,ragistor_no,fees,adress):
     data=read_csv
     new_rec={'Name':name,'Room_no':room_no,'Mobile_no':mobile_no,'Date':date,'Ragistor_no':ragistor_no,'Fees':fees,'Adress':adress}
     data.append(new_rec)
@@ -54,21 +52,54 @@ def read_record():
        writer.writerows(data)
 
 def addStudent(regNo,name,address,moNo,crsNm,sem,fee):
-  data=read_csv()
-  new_student={'regNo':regNo, 'name':name, 'address':address, 'moNo':moNo, 'csrNm':crsNm, 'sem':sem, 'fee':fee}
-  data.append(new_student)
-  write_csv(data)
+    data=read_csv()
+    new_student={'regNo':regNo, 'name':name, 'address':address, 'moNo':moNo, 'csrNm':crsNm, 'sem':sem, 'fee':fee}
+    data.append(new_student)
+    write_csv(data)
 
 def readStudent():
-  students=read_csv()
-  if not students:
-    print(" No Stduent Data found:")
+    students=read_csv()
+    if not students:
+        print(" No Stduent Data found:")
     return
-  headers=['regNo','name','address','moNo','csrNm','sem','fee']
-  col=widths={header:len(header) for header in headers}
-  for student in student:
-    for header in headers:
-      col_widths[header]=max(col_widths[header],len(student[header.lower().replace('','_')]))
-  separator='+-'+'-+-'.join(['-'*col_widths[header] for header in headers])+'|'
-  header_row='| '+' |'.join([header.ljust(col_widths[header]) for header in headers])+'|'
-  print(sepa)    
+
+
+    headers=['regNo','name','address','moNo','csrNm','sem','fee']
+    col=widths={header:len(header) for header in headers}
+    
+    for student in students:
+        for header in headers:
+            col_widths[header]=max(col_widths[header],len(student[header.lower().replace('','_')]))
+    separator='+-'+'-+-'.join(['-'*col_widths[header] for header in headers])+'|'
+    header_row='| '+' |'.join([header.ljust(col_widths[header]) for header in headers])+'|'
+    print(separator)
+    print(header_row) 
+    print(separator)
+    for student in students:
+        row='| '+' | '.join([student[header.lower().replace(' ','_')].ljust(col_widths[header])for header in headers]) + ' | '
+        print(row)
+    print(separator) 
+    
+def update_student(Regno,Name=None,Address=None,MoNo=None,CsrNm=None,Sem=None,Fee=None):
+    data=read_csv()
+    for student in data:
+        if student['regNo']== Regno :
+            if Name:
+                student['name']==Name
+            if Address:
+                student['address']==Address
+            if MoNo:
+                student['moNo']==MoNo
+            if CsrNm:
+                student['csrNm']==CsrNm
+            if Sem:
+                student=['sem']==Sem                
+            if Fee:
+                student=['fee']==Fee                
+            break
+    write_csv(data) 
+ 
+def delete_student(regNo):
+    data=read_csv()
+    data=[student for student in data if student['regNo'] != regNo] 
+    write_csv(data)                          
